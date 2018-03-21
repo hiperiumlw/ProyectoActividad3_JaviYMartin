c6ae5aea8f6809aaf196786a52cda0f95556a403
hiperiumlw <hiperiumtv@gmail.com>
2018-03-21 20:50:08 +0100

    Primer Commit Actividad3

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y apache2
+sudo apt-get update
+sudo apt-get install -y apache2
+DEBIAN_FRONTEND=noninteractive sudo -E apt-get -q -y install mysql-server
+sudo apt-get -y install php5
 if ! [ -L /var/www/html/ ]; then
   sudo rm -rf /var/www/html/
   sudo ln -fs /vagrant /var/www/html
 fi
+
+echo 'CREATE DATABASE IF NOT EXISTS actividad3 CHARACTER SET Latin1 COLLATE latin1_spanish_ci;'> basededatos.sql
+echo 'USE actividad3;' >> basededatos.sql
+echo 'CREATE TABLE cliente (id INT NOT NULL AUTO_INCREMENT,usuario VARCHAR(45) NOT NULL,email VARCHAR(45) NOT NULL,password VARCHAR(80) NOT NULL,hash VARCHAR(80) NULL,PRIMARY KEY (id));' >> basededatos.sql
+cat basededatos.sql
+sudo mysql -u root < basededatos.sql
+
--- public/assets/js/main.js
+++
@@ -1,6 +0,0 @@
-$('.btn-success').click(function () {
-    var usuario = $('#usuario').val();
-    console.log(usuario);
-    if (usuario==""){
-    }
-})
