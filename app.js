cea72ddecc3217804c48c84f10a34a6e393be3b2
Javier Pérez <jpv881@gmail.com>
2018-03-21 23:00:21 +0100

    commit

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const EXPRESS = require('express');
 const PATH = require('path');
 const APP = EXPRESS();
 
-APP.listen(3000, () => console.log("He levantado el puerto 3000"));
 
+
+APP.listen(3000, () => console.log("He levantado el puerto 3000"));
+//APP.get("/login", (req, res)=> res.send(__dirname+'/assets/css'));
 APP.use('/',EXPRESS.static(__dirname + '/'));
+
+APP.use('/assets', EXPRESS.static('assets'));
+// APP.use('/login', EXPRESS.static(__dirname + '/assets/html/login.html'));
+APP.get('/login',(req,res)=> res.sendFile(PATH.join(__dirname+'/assets/html/login.html')));
