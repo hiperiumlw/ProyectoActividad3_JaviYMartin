519d3cfb0afc27d7e15bb34222560f9e54064b6b
hiperiumlw <hiperiumtv@gmail.com>
2018-03-24 18:27:34 +0100

    Martin

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const EXPRESS = require('express');
 const PATH = require('path');
 const APP = EXPRESS();
 const MYSQL  = require('mysql');
+const BODYPARSER = require('body-parser');
+APP.use(BODYPARSER.json()); // support json encoded bodies
+APP.use(BODYPARSER.urlencoded({ extended: true })); // support encoded bodies
 
-
+const SHA1 = require('locutus/php/strings/sha1');
 
 APP.listen(3000, () => console.log("He levantado el puerto 3000"));
 //APP.get("/login", (req, res)=> res.send(__dirname+'/assets/css'));
@@ -19,7 +22,15 @@ APP.get('/registro',(req,res)=> res.sendFile(PATH.join(__dirname+'/assets/html/r
 APP.get('/login',(req,res)=> res.sendFile(PATH.join(__dirname+'/assets/html/login.html')));
 APP.get('/:destino',(req,res)=> res.sendFile(PATH.join(__dirname+'/assets/html/destinos/'+req.params.destino+'.html')));
 
-
+// POST http://localhost:8080/api/users
+// parameters sent with
+APP.get('/registro/usuario', function(req, res) {
+    var user_id = req.param('usuario');
+    var email = req.param('email');
+    var contraseña = req.param('contrasenya');
+    contraseña = SHA1(contraseña);
+    res.send(user_id + ' ' + email + ' ' + contraseña + ' '/* + repetircontraseña*/);
+});
 
 // var connection = MYSQL.createConnection({
 //     host: 'localhost',
