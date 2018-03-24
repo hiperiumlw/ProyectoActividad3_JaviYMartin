0e230387e515de69952960c2da7cde5647612da8
Javier Pérez <jpv881@gmail.com>
2018-03-24 12:58:21 +0100

    fin tareas

--- assets/js/login.js
+++ assets/js/login.js
@@ -5,19 +5,10 @@ $(".contrasenya-olvidada").on("click", function(){
     $("#divFormPass").fadeIn(2000);
 });
 
-$("#btnSend").on("click", function(){
-   $(".modal-body").empty();
-   $(".modal-body").append("<p>Mensaje enviado</p>");
-   $(".modal-footer").empty();
-   $(".modal-footer").append("<button id='btnAccept' class='btn btn-primary btn-sm'>Aceptar</button>");
-});
-
-$(document.body).on("click", "#btnAccept", function(){
-    //console.log(window.location.hostname+":"+window.location.port)
-    window.location.href = window.location.hostname+":"+window.location.port+"/";
-    return false;
-});
-
 $("#divBtnLogin button").on("click", function(){
+    let value = $("#user").val();
 
+    if(value !== ""){
+        localStorage.setItem("usuario", JSON.stringify({nombre: value}));
+    }
 });
